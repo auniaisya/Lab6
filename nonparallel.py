869b0425c75bb2d3fe8b78d2b30651699e81556c
auni <aisyahlim99@gmail.com>
2020-12-27 14:20:43 +0000

    lab6 commit

---
+++ nonparallel.py
@@ -0,0 +1,26 @@
+#i/user/bin/phyton
+
+from decimal import Decimal,getcontext
+from timeit import default_timer as timer
+
+def pi(precision):
+	getcontext().prec = precision
+	return sum(1/Decimal(16)**k *
+		Decimal(4)/(8*k+1) -
+		Decimal(2)/(8*k+4) -
+		Decimal(1)/(8*k+5) -
+		Decimal(1)/(8*k+6)) for k in range (precision))
+
+start = timer()
+values =(1000,1500,2000)
+data - list(map(pi,value))
+print(data[0])
+print("")
+print (data[1])
+print("")
+print(data[2])
+
+end = timer()
+print("")
+print(f'sequentially:{end - start}')
+.
